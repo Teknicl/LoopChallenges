68100735324fa41886dfa7d9fa91b43f86f57287
Torrei Whitfield <bossbollin@msn.com>
2023-01-11 19:38:01 -0800

    Completed assignment

---
+++ script.js
@@ -0,0 +1,30 @@
+// Print odds 1-20
+for(var i = 1; i<20; i+=2) {
+    console.log(i);
+}
+
+// Decreasing muliples of 3
+for(var i = 100; i>0; i--) {
+    if(i % 3 == 0 ) {
+        console.log(i);
+    }
+}
+
+// Print the sequence
+for(var i = 4; i>-4; i-=1.5) {
+    console.log(i);
+}
+
+// Sigma
+var sum = 0;
+for(var i=1; i<=100; i++) {
+    sum += i;
+}
+console.log(sum);
+
+//Factorial
+var product = 1;
+for(var i=1; i<=12; i++) {
+    product = i * product;
+}
+console.log(product);
